b5804a4f4d6a72bd31ca53e91a44576fee771839
matt-farmer <solversurfer@icloud.com>
2018-07-09 15:52:58 +1000

    complete dummy manifest
    has injected values but shows whole concept

--- src/components/LcManifest.vue
+++ src/components/LcManifest.vue
@@ -1,6 +1,8 @@
 
 <template>
-    <q-card inline class="bg-white" style="width: 500px">
+  <div class="row">
+    <div class="col-6 q-pr-md">
+    <q-card square class="bg-white">
       <ChallengeImageChooser></ChallengeImageChooser>
       <q-card-separator />
       <q-card-title >
@@ -33,10 +35,12 @@
       </q-card-main>
       <q-card-separator />
       <q-card-main>
-        <div class="row text-faded">
-          <div class="col-5 q-subheading">Curriculum Alignment:</div>
-          <div class="col-auto">
-            <span class="q-subheading" v-for="(statement, index) in selectedStatements" :key="statement">
+        <div class="row">
+          <div class="col-auto q-subheading text-faded">Curriculum Alignment:</div>
+        </div>
+        <div class="row">
+          <div class="col-12">
+            <span class="q-subheading text-faded" v-for="(statement, index) in selectedStatements" :key="statement">
               {{ statement }}
               <span v-if="index < selectedStatements.length - 1">
                 -
@@ -50,7 +54,7 @@
         <q-list-header>Resources:</q-list-header>
         <q-item tag="label" v-for="resource in selectedResources" :key="resource">
           <q-item-side>
-            <q-icon name="open_in_new" style="font-size: 35px"/>
+            <q-icon name="open_in_new" style="font-size: 25px"/>
           </q-item-side>
           <q-item-main>
             <q-item-tile sublabel>
@@ -64,6 +68,79 @@
       <q-card-actions>
       </q-card-actions>
     </q-card>
+  </div>
+  <div class="col-6">
+    <q-card square class="bg-white">
+      <q-card-title>
+        <q-icon class="q-pr-md" name="people" style="font-size: 25px"/>People
+      </q-card-title>
+      <q-card-main>
+        <q-list dense>
+          <q-list-header>Collaborate:</q-list-header>
+          <q-item tag="label" v-for="teacher in collaborators" :key="teacher">
+            <q-item-side>
+              <!-- <q-icon name="open_in_new" style="font-size: 25px"/> -->
+            </q-item-side>
+            <q-item-main>
+              <q-item-tile sublabel>{{ teacher }}
+              </q-item-tile>
+            </q-item-main>
+            <q-item-side>
+            </q-item-side>
+          </q-item>
+          <q-list-header>Notify:</q-list-header>
+          <q-item tag="label" v-for="teacher in notifies" :key="teacher">
+            <q-item-side>
+              <!-- <q-icon name="open_in_new" style="font-size: 25px"/> -->
+            </q-item-side>
+            <q-item-main>
+              <q-item-tile sublabel>{{ teacher }}
+              </q-item-tile>
+            </q-item-main>
+            <q-item-side>
+            </q-item-side>
+          </q-item>
+        </q-list>
+      </q-card-main>
+      <q-card-separator />
+      <q-card-title>
+        <q-icon class="q-pr-md" name="assessment" style="font-size: 25px"/>Assessment
+      </q-card-title>
+      <q-card-main>
+        <p>Rubric has been created</p>
+        <p>1 Long-form submission, 3 quizzes</p>
+      </q-card-main>
+      <q-card-separator />
+      <q-card-title>
+        <q-icon class="q-pr-md" name="timeline" style="font-size: 25px"/>Timeline
+      </q-card-title>
+      <q-card-main>
+        <q-list dense>
+          <q-list-header>Use With:</q-list-header>
+          <q-item tag="label" v-for="time in timelines" :key="time.class">
+            <q-item-side>
+              <!-- <q-icon name="open_in_new" style="font-size: 25px"/> -->
+            </q-item-side>
+            <q-item-main>
+              <q-item-tile sublabel>{{ time.class }}</q-item-tile>
+            </q-item-main>
+            <q-item-side>
+              <q-item-tile sublabel>{{ time.time }}</q-item-tile>
+            </q-item-side>
+          </q-item>
+        </q-list>
+      </q-card-main>
+      <q-card-separator />
+      <q-card-title>
+        <q-icon class="q-pr-md" name="assessment" style="font-size: 25px"/>Actions
+      </q-card-title>
+      <q-card-actions align="end">
+        <q-btn flat color="primary" label="Deploy"/>
+        <q-btn flat color="primary" label="Monitor"/>
+      </q-card-actions>
+    </q-card>
+  </div>
+  </div>
 </template>
 
 <script>
@@ -83,7 +160,15 @@ export default {
     // LcManifest: LcManifest
   },
   data () {
-    return {}
+    return {
+      // temp data until people service active
+      collaborators: ['Mr Geology', 'Mrs Science'],
+      notifies: ['Mrs Physics', 'Mrs Maths'],
+      timelines: [
+        {class: '7b', time: 'Wednesday 10:00 am'},
+        {class: '7f', time: 'Friday 11:30 am'}
+      ]
+    }
   },
   methods: {
     resourceLinkHandler: function (resource) {
